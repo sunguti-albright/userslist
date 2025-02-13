a0cead3a04ec936ca6fef5f081a26a51b1bad317
Albright Sunguti <prudencealbright01@gmail.com>
2025-02-13 16:36:01 +0300

    chore:filter function and minor ui fixes

--- src/app/components/userslist/userslist.component.ts
+++ src/app/components/userslist/userslist.component.ts
@@ -1,27 +1,41 @@
 import { Component, OnInit } from '@angular/core';
 import { UsersService } from '../../services/users.service';
-import { Comment } from '@angular/compiler';
+import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-userslist',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './userslist.component.html',
-  styleUrl: './userslist.component.scss'
+  styleUrl: './userslist.component.scss',
 })
 export class UserslistComponent implements OnInit {
-usersList:any=[]
-  constructor(private usersService : UsersService){}
+  usersList: any[] = [];
+  filteredUsers: any[] = [];
+  filterBy: string = '';
+
+  constructor(private usersService: UsersService) {}
 
   ngOnInit(): void {
-    this.fetchUsers()
+    this.fetchUsers();
   }
 
-fetchUsers(){
-  return this.usersService.getUsers().subscribe(users =>{
-    this.usersList = users
-    console.log('users>>>>>', this.usersList)
-  })
-}
+  //list all users oninit
+  fetchUsers() {
+    this.usersService.getUsers().subscribe((users) => {
+      this.usersList = users;
+      this.filteredUsers = [...this.usersList]; 
+    });
+  }
+
+  filter() {
+    if (!this.filterBy) {
+      this.filteredUsers = [...this.usersList]; // If search bar is empty, show all users
+    } else {
+      this.filteredUsers = this.usersList.filter((user: any) =>
+        user.name.toLowerCase().includes(this.filterBy.toLowerCase())
+      );
+    }
+  }
 }
