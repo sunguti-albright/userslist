04b3b6f7d24f96deaf948c7e47d5be1bf4f9943e
Albright Sunguti <prudencealbright01@gmail.com>
2025-02-13 16:34:09 +0300

    users service

--- src/app/services/users.service.ts
+++ src/app/services/users.service.ts
@@ -1,15 +1,17 @@
-import { HttpClient } from '@angular/common/http';
+
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UsersService {
-usersUrl = 'https://jsonplaceholder.typicode.com/users';
-  constructor(private http:HttpClient) { }
+  usersUrl = 'https://jsonplaceholder.typicode.com/users';
 
-  getUsers(): Observable<any>{
-    return this.http.get<any>(this.usersUrl)
+  constructor(private http: HttpClient) {}
+
+  getUsers(): Observable<any> {
+    return this.http.get<any>(this.usersUrl);
   }
 }
